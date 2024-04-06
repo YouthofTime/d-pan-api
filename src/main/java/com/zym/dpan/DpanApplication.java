f99a26beb7875f3ed9ae6694bf7043abc178216e
zym <2698805071@qq.com>
2024-04-06 19:24:39 +0800

    SpringBoot项目初始化构建

---
+++ src/main/java/com/zym/dpan/DpanApplication.java
@@ -0,0 +1,13 @@
+package com.zym.dpan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpanApplication.class, args);
+	}
+
+}
