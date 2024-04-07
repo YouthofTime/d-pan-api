28381292ec7bce9c8d38433c1b5389fa2bc39f44
zym <2698805071@qq.com>
2024-04-07 20:56:55 +0800

    项目逆向生成、统一异常处理、秒传文件

--- src/main/java/com/zym/dpan/DpanApplication.java
+++ src/main/java/com/zym/dpan/DpanApplication.java
@@ -1,9 +1,11 @@
 package com.zym.dpan;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.zym.dpan.dao")
 public class DpanApplication {
 
 	public static void main(String[] args) {
